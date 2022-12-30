f8319e7f39fd1b6c0a88fa33a380ee116cd9ccca
Vaibhav N <vn202207@nexttechvision.com>
2022-12-30 11:49:39 +0530

    swagger config

---
+++ src/main/java/com/codekul/springdec6/swaggerconfig/SpringFoxConfiguration.java
@@ -0,0 +1,16 @@
+package com.codekul.springdec6.swaggerconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class SpringFoxConfiguration {
+//
+//    @Bean
+//    public Docket swaggerApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select().apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//}
